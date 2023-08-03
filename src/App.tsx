3cde645e7ba3626d7157e7e8ea3ed34c84e8af59
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2023-08-03 12:45:21 +0300

    add width to container

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     dispatch(setModalType('create'));
     dispatch(setShowModal());
   };
-  return <div className="mr-auto ml-auto px-5 pb-5">
+  return <div className="mr-auto ml-auto max-w-4xl px-5 pb-5">
     <Table> <HeaderTableNotes /> </Table>
     <button className="py-1.5 px-2.5 bg-green-500 text-white font-medium rounded-md hover:bg-green-600 focus:bg-green-600" onClick={handleCreateNote}>Create Note</button>
     <Table> <HeaderTableNotesSummaryNotes /> </Table>
